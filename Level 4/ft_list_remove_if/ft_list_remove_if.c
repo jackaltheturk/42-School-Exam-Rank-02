5b01df1ec3ee1dd6d75afc3288f9a1655a3dd48f
Erman TORUN <74808142+jackaltheturk@users.noreply.github.com>
2025-01-04 10:46:52 +0300

    Update ft_list_remove_if.c
    line 21 wasnt needed if I am not wrong

--- Level 4/ft_list_remove_if/ft_list_remove_if.c
+++ Level 4/ft_list_remove_if/ft_list_remove_if.c
@@ -18,7 +18,6 @@ void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 	}
 	else // if there is a no else, you cant pass the Moulinette, tryed 2023.09.08
 	{
-		cur = *begin_list;
 		ft_list_remove_if(&cur->next, data_ref, cmp);
 	}
 }
